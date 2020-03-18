7cf1a31c1dc01655bf277ef48c7a4361771e48ca
Varun Sukheja <varun.sukheja@myglamm.com>
2020-03-19 01:25:22 +0530

    added new component burger builder

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Aux from '../../hoc/Aux';
+// import classes from './Layout.module.scss';
+import classes from './Layout.module.scss';
 
 const layout = props => (
   <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <div className={classes.content}>Toolbar, SideDrawer, Backdrop</div>
     <main>{props.children}</main>
   </Aux>
 );
