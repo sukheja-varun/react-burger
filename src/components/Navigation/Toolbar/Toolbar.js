2a7013b34e39c11beb5881539463e0d816cac4e7
Varun Sukheja <varun.sukheja@myglamm.com>
2020-03-23 19:42:40 +0530

    added logo

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import classes from './Toolbar.module.scss';
+import Logo from '../../Logo/Logo';
 
 const toolbar = () => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <div>LOGO</div>
+    <Logo />
     <nav>...</nav>
   </header>
 );
